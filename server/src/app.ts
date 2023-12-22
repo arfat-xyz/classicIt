888d334b87da835e5e7895f88bccc6828f09305b
arfat-xyz <arfatrahman08@gmail.com>
2023-12-22 19:40:10 +0600

    first commit

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { Application } from "express";
+const app: Application = express();
+app.use(cors());
+// parser
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to my Database");
+});
+
+export default app;
